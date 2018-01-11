bd80a44beb6f68a7da4f9ecf51e2dffafd5c3fd4
Allen Kao <hushkao@gmail.com>
2018-01-11 15:39:19 +0100

    Update source of perl script

--- MY/extract_sequence/runMe.sh
+++ MY/extract_sequence/runMe.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 wget -nc https://www.arabidopsis.org/download_files/Sequences/TAIR10_blastsets/TAIR10_cds_20101214_updated
 
-wget -nc http://psbweb03.psb.ugent.be/~shkao/public/fastaqual_select.pl
+wget -nc https://raw.githubusercontent.com/shkao/TRBL/master/MY/extract_sequence/fastaqual_select.pl
 
 perl fastaqual_select.pl -fastafile TAIR10_cds_20101214_updated -include HKG_list.txt > HKG_Arabidopsis.fasta
