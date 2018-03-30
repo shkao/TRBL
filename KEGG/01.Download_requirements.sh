a289d6cd7abb46e40065b18c41f5bc62468e5684
Allen Kao <hushkao@gmail.com>
2018-03-30 15:00:06 +0200

    Tidy directories for KEGG stuff

--- KEGG/01.Download_requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-
-mkdir -p data
-
-wget -P data -nc http://www.genome.jp/kegg/pathway.html
-wget -nc -O "data/KO.keg" "http://www.genome.jp/kegg-bin/download_htext?htext=ko00001.keg&format=htext&filedir="
